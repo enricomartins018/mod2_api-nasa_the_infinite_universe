bdfd0dc70ff04ce15dd02cba2b888b110966c773
enricomartins018 <enrico.martins018@gmail.com>
2022-05-12 17:14:05 -0300

    alterando o site para ficar em uma página só

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-$('#submit').click(function (e){
-    e.preventDefault()
+$('#submit').click(function (event){
+    event.preventDefault()
     var dateInput = $('#birthdayInput').val()
     $.ajax({
     url: `https://api.nasa.gov/planetary/apod?api_key=pYIbv70Np8uaQJDIZumLpNB2oOJWcChnW7gwsgk9&date=${dateInput}`,
@@ -14,3 +14,19 @@ $('#submit').click(function (e){
     }
 })
 })
+
+function showsImg(answer) {
+    $('#imgDiv').css('display', 'flex')
+    $('#vidDiv').css('display', 'none')
+    $('#img-api').attr("src", answer.url)
+    $('#container-media_h2').text(answer.title)
+    $('#container-media_p').text(answer.explanation)
+}
+
+function showsVid(answer) {
+    $('#vidDiv').css('display', 'flex')
+    $('#imgDiv').css('display', 'none')
+    $('#vid-api').attr("src", answer.url)
+    $('#container-media_h2').text(answer.title)
+    $('#container-media_p').text(answer.explanation)
+}
