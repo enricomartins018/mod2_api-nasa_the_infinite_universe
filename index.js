6909e98a2f9c595a9e093d21c6614b2405114638
enricomartins018 <enrico.martins018@gmail.com>
2022-05-12 17:40:44 -0300

    adicionando o js na minha API

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ $('#submit').click(function (event){
     url: `https://api.nasa.gov/planetary/apod?api_key=pYIbv70Np8uaQJDIZumLpNB2oOJWcChnW7gwsgk9&date=${dateInput}`,
     success: function (answer) {
         if (answer.media_type == "video") {
-            console.log('entrou')
-            console.log(answer)
             showsVid(answer)
         } else {
             showsImg(answer)
